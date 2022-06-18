6cb203f38697e5d4581649198f179ed19fa0b3b1
Nazmul Huda shanto <105204877+Shanto37@users.noreply.github.com>
2022-06-18 15:22:52 +0600

    Add files via upload

---
+++ exercise4.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+	int number,result;
+
+	printf("Enter the number:");
+	scanf("%d",&number);
+
+	result=number+11111;
+
+	printf("Result:%d",result);
+
+
+	return 0;
+
+
+}
