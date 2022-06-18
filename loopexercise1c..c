79247808eda8c970b597825888f75285b7bd4723
Nazmul Huda shanto <105204877+Shanto37@users.noreply.github.com>
2022-06-18 15:56:18 +0600

    Add files via upload

---
+++ loopexercise1c..c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+
+	int i;
+
+	for(i=0;i<100;i++){
+
+		if(i==5){
+
+			break;
+		}
+
+		printf("Angel\n");
+
+	int i=1;
+
+	while(i<11)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+
+	printf("End of for loop");
+
+	return 0;
+
+
+
+
+
+
+}
+
+
+
+}
+
---
+++ loopexercise4.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main()
+{
+	int i;
+
+	for(i=10;i>0;i--){
+
+		printf("%d\n",i);
+	}
+
+
+	return 0;
+
+
+
+
+
+
+}
